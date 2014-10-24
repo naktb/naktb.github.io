a23ca1c473b213a94f0db7cd74ec365055914b59
Miroslav Martynov <marty.mircha@gmail.com>
2014-10-24 18:29:57 +0300

    Added call back form to price page. Removed scroll from price and who we are pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 options: {
-                    outputStyle: 'expanded'
+                    outputStyle: 'compressed'
                 },
                 files: {
                     'css/app.css': 'scss/app.scss'
