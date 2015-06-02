e4858964b8be8dc1117f9ecd97ef7d2cd4df6bbf
Miroslav Martynov <marty.mircha@gmail.com>
2015-06-02 18:36:22 +0300

    Resolve node version, update grunt, added browsersync.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,80 +1,58 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        htmlbuild: {
-            dist: {
-                src: 'pages/**/*.html',
-                dest: '_site/',
-                options: {
-                    beautify: true,
-//                    prefix: '//some-cdn',
-                    relative: true,
-//                scripts: {
-//                    bundle: [
-//                        '<%= fixturesPath %>/scripts/*.js',
-//                        '!**/main.js',
-//                    ],
-//                    main: '<%= fixturesPath %>/scripts/main.js'
-//                },
-                    styles: {
-                        app: [
-                            '<%= compiledSite %>/css/print.css',
-                            '<%= compiledSite %>/css/screen.css'
-                        ]
-                    },
-                    sections: {
-                        views: '<%= fixturesPath %>/views/**/*.html',
-                        templates: '<%= fixturesPath %>/templates/**/*.html',
-                        layout: {
-                            head: '<%= fixturesPath %>/layout/head.html',
-                            header: '<%= fixturesPath %>/layout/header.html',
-                            match_centr_top: '<%= fixturesPath %>/layout/match_centr_top.html',
-                            top_menu: '<%= fixturesPath %>/layout/top_menu.html',
-                            logo_section: '<%= fixturesPath %>/layout/logo_section.html',
-                            logo_section_02: '<%= fixturesPath %>/layout/logo_section_02.html',
-                            second_menu: '<%= fixturesPath %>/layout/second_menu.html',
-                            second_menu_02: '<%= fixturesPath %>/layout/second_menu_02.html',
-                            footer: '<%= fixturesPath %>/layout/footer.html',
-                            sidebar: '<%= fixturesPath %>/layout/sidebar.html',
-                            sidebar_02: '<%= fixturesPath %>/layout/sidebar_02.html'
-                        }
-                    },
-                    data: {
-                        // Data to pass to templates
-                        version: "0.1.0",
-                        title: "test"
-                    }
-                }
-            }
-        },
-        sass: {
-            options: {
-                includePaths: ['bower_components/foundation/scss']
-            },
-            dist: {
-                options: {
-                    outputStyle: 'compressed'
-                },
-                files: {
-                    'css/app.css': 'scss/app.scss'
-                }
-            }
-        },
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        watch: {
-            grunt: { files: ['Gruntfile.js'] },
-
-            sass: {
-                files: 'scss/**/*.scss',
-                tasks: ['sass']
-            }
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sourceMap: true,
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-html-build');
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
 
-    grunt.registerTask('build', ['sass']);
-    grunt.registerTask('default', ['build', 'watch']);
-}
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : [
+          'css/app.css',
+          '*.html',
+          'js/*.js'
+        ]
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "./"
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['browserSync','watch']);
+}			
