ab2bfdf90f617efd2a46165c4fc58c19da9a0425
Miroslav Martynov <marty.mircha@gmail.com>
2015-06-07 19:02:43 +0300

    setup html build

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,47 @@ module.exports = function(grunt) {
           baseDir: "./_site"
         }
       }
+    },
+
+    fixturesPath: "_site",
+
+    htmlbuild: {
+      dist: {
+        src: 'index.html',
+        dest: '/',
+        options: {
+          beautify: true,
+          prefix: '//some-cdn',
+          relative: true,
+          scripts: {
+            bundle: [
+              '<%= fixturesPath %>/scripts/*.js',
+              '!**/main.js',
+            ],
+            main: '<%= fixturesPath %>/scripts/main.js'
+          },
+          styles: {
+            bundle: [
+              '<%= fixturesPath %>/css/libs.css',
+              '<%= fixturesPath %>/css/dev.css'
+            ],
+            test: '<%= fixturesPath %>/css/inline.css'
+          },
+          sections: {
+            views: '<%= fixturesPath %>/views/**/*.html',
+            templates: '<%= fixturesPath %>/templates/**/*.html',
+            layout: {
+              header: '<%= fixturesPath %>/layout/header.html',
+              footer: '<%= fixturesPath %>/layout/footer.html'
+            }
+          },
+          data: {
+            // Data to pass to templates
+            version: "0.1.0",
+            title: "test",
+          },
+        }
+      }
     }
 
   });
